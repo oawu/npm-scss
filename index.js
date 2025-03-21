b7cc8431b9e3961c34c08529137ba281e92e16f3
OA Wu <oawu.tw@gmail.com>
2025-03-21 18:35:37 +0800

    no message

--- index.js
+++ index.js
@@ -1,11 +1,92 @@
 /**
  * @author      OA Wu <oawu.tw@gmail.com>
- * @copyright   Copyright (c) 2015 - 2022, @oawu/scss
+ * @copyright   Copyright (c) 2015 - 2025, @oawu/scss
  * @license     http://opensource.org/licenses/MIT  MIT License
  * @link        https://www.ioa.tw/
  */
 
-const watch = require('./watch.js')
-const scss  = require('./scss.js')
+const Path = require('path')
+const _sass = require('sass')
+const fs = require('fs/promises')
 
-module.exports = { ...scss, watch }
+const files = []
+
+const { tryIgnore, closureOrPromise, Type: T } = require('@oawu/helper')
+
+let _contents = null
+
+const Scss = function (type, done = null) {
+  return closureOrPromise(done, done => _sass.render({
+    importer: (url, file, done) => {
+      (async (url, file) => {
+        if (_contents === null) {
+          _contents = await fs.readFile(__dirname + Path.sep + 'Libs' + Path.sep + 'Lalilo.scss', { encoding: 'utf8' })
+        }
+
+        if (['Lalilo', '*'].includes(url) && _contents !== null) {
+          return { contents: _contents }
+        }
+
+        const contents = ''
+        const tokens = url.split('/').map(token => token.trim()).filter(token => token !== '')
+
+        if (!tokens.length) {
+          return { contents }
+        }
+
+        if (tokens.shift() == Scss.key) {
+
+          if (file = __dirname + Path.sep + 'Libs' + Path.sep, tokens.length) {
+            file += tokens.join(Path.sep)
+          } else {
+            file += Scss.key
+          }
+        } else {
+          file = Path.dirname(file) + Path.sep + url
+        }
+
+        if (Path.extname(file) !== '.scss') {
+          file += '.scss'
+        }
+
+        if (files.includes(file)) {
+          return { file }
+        }
+
+        const access = await tryIgnore(fs.access(file, fs.constants.R_OK))
+        if (T.err(access)) {
+          throw new Error(`@import 的 "${url}" 沒有訪問權限。`, { cause: access })
+        }
+
+        const stats = await fs.stat(file)
+        if (!stats.isFile()) {
+          throw new Error(`@import 的 "${url}" 不是檔案類型。`)
+        }
+
+        files.push(file)
+
+        return { file }
+
+      })(url, file).then(done).catch(done)
+    },
+    outputStyle: Scss.minify
+      ? 'compressed'
+      : 'expanded',
+    ...type,
+  }, (error, result) => {
+    if (T.err(error)) {
+      error.info = error.formatted.split("\n").shift()
+      return done(error)
+    }
+
+    result.utf8 = (Scss.minify ? '@charset "UTF-8";' + "\n" : '') + result.css.toString()
+    return done(result)
+  }))
+}
+
+Scss.key = '@'
+Scss.minify = true
+Scss.file = (file, done = null) => Scss({ file }, done)
+Scss.data = (data, done = null) => Scss({ data }, done)
+
+module.exports = Scss
--- test.js
+++ test.js
@@ -1,26 +1,45 @@
 /**
  * @author      OA Wu <oawu.tw@gmail.com>
- * @copyright   Copyright (c) 2015 - 2022, @oawu/xterm
+ * @copyright   Copyright (c) 2015 - 2025, @oawu/scss
  * @license     http://opensource.org/licenses/MIT  MIT License
  * @link        https://www.ioa.tw/
  */
 
+const fs = require('fs/promises')
+const { Type: T } = require('@oawu/helper')
+
 const Scss = require('./index.js')
-const FileSystem = require('fs')
 
 Scss.minify = false
-Scss.file('test/test.scss', (error, result) => {
-  if (error) {
-    console.error('info：' + error.info);
-    console.error('line：' + error.line);
-    console.error('column：' + error.column);
-    console.error('message：' + error.message);
+
+const src = 'test/test.scss'
+const desc = 'test/test.css'
+
+Scss.file(src, result => {
+  if (T.err(result)) {
+    console.error('info：' + result.info)
+    console.error('line：' + result.line)
+    console.error('column：' + result.column)
+    console.error('message：' + result.message)
     return
   }
-  
-  console.error(result.stats.duration);
+  console.error(result.stats.duration)
 
-  FileSystem.writeFile("test/test.css", result.utf8, 'utf8', error => {
-    console.error('ok');
-  })
+  ;(async src => {
+    const result = await Scss.file(src)
+
+    if (T.err(result)) {
+      console.error('info：' + result.info)
+      console.error('line：' + result.line)
+      console.error('column：' + result.column)
+      console.error('message：' + result.message)
+      return
+    }
+
+    console.error(result.stats.duration)
+
+    await fs.writeFile(desc, result.utf8, { encoding: 'utf8' })
+    console.error('ok')
+  })(src).catch(console.error)
 })
+
